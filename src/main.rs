71f1c3004bd6e40700339e868ea8b11766eff04f
Samuel Gosselin <gosselin.sam@gmail.com>
2021-02-01 22:12:06 -0800

    main: add tui dependencies.

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,39 @@
-// TODO: include these modules here so sub-modules can find them
-// via crate::.  I'm not sure whether there is a cleaner way to do
-// this.
+use clap::{Arg, App};
+
+// Include these modules here so sub-modules can find them via crate::.  I am not
+// sure whether there is a cleaner way to do this.
 mod cpu;
 mod dbg;
 mod mem;
 mod vid;
 
-fn main() {
+fn app_tui() {
+    // TODO.
+}
+
+fn app_cli() {
     let mut cpu = cpu::Cpu::new();
     let mut mmu = mem::Mmu::new();
 
     loop {
         let ncycles = cpu.step(&mut mmu);
         mmu.gpu.step(ncycles);
+    }
+}
+
+fn main() {
+    let matches = App::new("rgb")
+        .version("0.1.0")
+        .author("Samuel Gosselin")
+        .arg(Arg::with_name("tui")
+            .long("tui")
+            .multiple(false)
+            .help("start a terminal based ui"))
+        .get_matches();
+
+    if matches.is_present("tui") {
+        app_tui();
+    } else {
+        app_cli();
     }
 }
