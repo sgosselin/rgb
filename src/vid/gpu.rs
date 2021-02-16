d202777d08113c2a99337640e497b585ca069b95
Samuel Gosselin <gosselin.sam@gmail.com>
2021-02-16 15:14:39 -0800

    ppu: add state diagrams for modes & transitions.

--- src/vid/gpu.rs
+++ src/vid/gpu.rs
@@ -2,16 +2,53 @@ use crate::dbg::log;
 
 /// Enumerates the possible mode for the GPU.
 ///
-/// Mode0: TODO
-/// Mode1: TODO
-/// Mode2: TODO
-/// Mode3: TODO
+/// ## Description of each modes
+///
+/// Mode 2: OAM Scan
+/// TODO
+///
+/// Mode 3: Drawing
+/// TODO
+/// Mode 0: HBlank
+/// TODO
+///
+/// Mode 1: VBlank
+/// TODO
+///
+///
+/// ## State diagram of the modes and their transitions:
+///
+///            +-------------------------+
+///            |                         +No
+///  +---------v--------+        XXXXXXXXXXXXXXX  Yes    +-----------------------+
+///  |  Mode 1: VBlank  +------>X  LY == 153 ?  X+--+--->+   Mode 2: OAM Search  +<--+
+///  +---------+--------+        XXXXXXXXXXXXXXX    ^    +------------+----------+   |
+///            ^                                    |                 |              |
+///            |                                    |                 v              |
+///            +Yes                                 |            XXXXXXXXXX  No      |
+///     XXXXXXXXXXXXXXX  No                         |           X  Done ?  X+--------+
+///    X  LY == 144 ?  X+---------------------------+            XXXXXXXXXX
+///     XXXXXXXXXXXXXXX                                               +Yes
+///            ^                                                      |
+///            |                                                      v
+///            +Yes                                        +----------+-----------+
+///        XXXXXXXXXX  No                                  |    Pixel Transfer    +<--+
+///       X  Done ?  X+---------+                          +----------+-----------+   |
+///        XXXXXXXXXX           |                                     |               |
+///            ^                |                                     v               |
+///            |                |                             XXXXXXXXXXXXXXX  No     |
+/// +----------+------------+   |                            X  LX == 160 ?  X+-------+
+/// |    Mode 0: HBlank     +<--+                             XXXXXXXXXXXXXXX
+/// +----------+------------+                                         +Yes
+///            ^                                                      |
+///            +------------------------------------------------------+
+///
 #[derive(Copy, Clone)]
 enum Mode {
-    Mode0,
-    Mode1,
-    Mode2,
-    Mode3,
+    HBlank,     // Mode 0
+    VBlank,     // Mode 1
+    OamScan,    // Mode 2
+    Drawing,    // Mode 3
 }
 
 /// Represents the GameBoy's GPU; also known as PPU for
@@ -25,7 +62,7 @@ impl Gpu {
     /// Creates a new GPU object.
     pub fn new() -> Gpu {
         return Gpu {
-            curr_mode: Mode::Mode0,
+            curr_mode: Mode::OamScan,
         };
     }
 
@@ -34,5 +71,11 @@ impl Gpu {
         // TODO.
 
         log::info("gpu", "step", &format!("ncycles={}", ncycles));
+    }
+
+
+    /// Copies the screen region of the internal framebuffer.
+    pub fn copy_screen(&self, dst: &[u8]) {
+        // TODO
     }
 }
